bdf127ee02929db7f358a855dd819017085a78eb
keshanpu <keshanpu@126.com>
2016-09-18 20:52:25 +0800

    Merge branch 'master' of http://github.com/zs811128838/BeautifulDiscovery
    # Conflicts:
    #	.idea/gradle.xml
    #	refreshlibrary/build/intermediates/bundles/debug/classes.jar
    #	refreshlibrary/build/intermediates/bundles/release/classes.jar
    #	refreshlibrary/build/intermediates/incremental/mergeDebugAndroidTestAssets/merger.xml
    #	refreshlibrary/build/intermediates/incremental/mergeDebugAndroidTestResources/merger.xml
    #	refreshlibrary/build/intermediates/incremental/mergeDebugAndroidTestShaders/merger.xml
    #	refreshlibrary/build/intermediates/incremental/mergeDebugAssets/merger.xml
    #	refreshlibrary/build/intermediates/incremental/mergeDebugJniLibFolders/merger.xml
    #	refreshlibrary/build/intermediates/incremental/mergeDebugShaders/merger.xml
    #	refreshlibrary/build/intermediates/incremental/mergeReleaseAssets/merger.xml
    #	refreshlibrary/build/intermediates/incremental/mergeReleaseJniLibFolders/merger.xml
    #	refreshlibrary/build/intermediates/incremental/mergeReleaseShaders/merger.xml
    #	refreshlibrary/build/intermediates/incremental/packageDebugResources/merger.xml
    #	refreshlibrary/build/intermediates/incremental/packageReleaseResources/merger.xml
    #	refreshlibrary/build/intermediates/res/resources-debug-androidTest.ap_
    #	refreshlibrary/build/outputs/aar/refreshlibrary-debug.aar
    #	refreshlibrary/build/outputs/aar/refreshlibrary-release.aar

--- app/src/main/java/com/android/beautifulthing/CommonActivity/LoginActivity.java
+++ app/src/main/java/com/android/beautifulthing/CommonActivity/LoginActivity.java
@@ -5,18 +5,33 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.beautifulthing.R;
+import com.android.beautifulthing.database.DatabaseTool;
 
 /**
  * Created by ydy on 2016/9/18.
  */
 public class LoginActivity extends AppCompatActivity{
 
+    public EditText username;
+    public EditText password;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_view);
+        initView();
+    }
+
+    private void initView() {
+        username = (EditText) findViewById(R.id.login_username);
+        password = (EditText) findViewById(R.id.login_password);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        username.setText(name);
     }
 
     public void click(View view){
@@ -27,6 +42,18 @@ public class LoginActivity extends AppCompatActivity{
             case R.id.login_turn_to_register://跳转到注册界面
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 LoginActivity.this.startActivity(intent);
+                break;
+            case R.id.login_login_btn:
+                //登陆按钮
+                String name = username.getText().toString();
+                String pwd = password.getText().toString();
+                boolean flag = DatabaseTool.login(this, name, pwd);
+                if(flag){
+                    Toast.makeText(this,"登陆成功",Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(this,"登陆失败",Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
     }
--- app/src/main/java/com/android/beautifulthing/CommonActivity/RegisterActivity.java
+++ app/src/main/java/com/android/beautifulthing/CommonActivity/RegisterActivity.java
@@ -1,22 +1,36 @@
 package com.android.beautifulthing.CommonActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.beautifulthing.R;
+import com.android.beautifulthing.database.DatabaseTool;
 
 /**
  * Created by ydy on 2016/9/18.
  */
 public class RegisterActivity extends AppCompatActivity{
 
+    public EditText username;
+    public EditText password;
+    public EditText repeat_password;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_view);
+        initView();
+    }
+
+    private void initView() {
+        username = (EditText) findViewById(R.id.register_username);
+        password = (EditText) findViewById(R.id.register_password);
+        repeat_password = (EditText) findViewById(R.id.register_repeat_password);
     }
 
     public void click(View view){
@@ -25,7 +39,18 @@ public class RegisterActivity extends AppCompatActivity{
                 finish();
                 break;
             case R.id.register_register_btn://注册
-                Toast.makeText(RegisterActivity.this, "注册...", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(RegisterActivity.this, "注册...", Toast.LENGTH_SHORT).show();
+
+                String name = username.getText().toString();
+                String pwd = password.getText().toString();
+                String repeatPwd = repeat_password.getText().toString();
+                boolean flag = DatabaseTool.register(this, name, pwd, repeatPwd);
+                if (flag){
+                    Toast.makeText(this,"注册成功",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    intent.putExtra("username",name);
+                    RegisterActivity.this.startActivity(intent);
+                }
                 break;
         }
     }
